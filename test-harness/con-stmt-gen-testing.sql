56d642c2b9dfd869b579a7e197af2bf8d95bfaa6
Rich Stewart <rich.stewart@lifeway.com>
2020-02-21 14:44:35 -0600

    Creating a package, and "working tables," to hold test data generated by invoking the statement generation processes.  This is a work-in-progress which tries to use the autonomous transactions feature of Oracle to help preserve the generated test data.

--- test-harness/con-stmt-gen-testing.sql
+++ test-harness/con-stmt-gen-testing.sql
@@ -1,5 +1,5 @@
 -- -*- tab-width: 4 indent-tabs-mode: nil -*-
-/* 
+/*
  Within the rstewar schema:
 */
 create table tst_lwx_ar_stmt_headers as select * from lwx.lwx_ar_stmt_headers where 0=1;
@@ -10,6 +10,3 @@ grant select,insert,delete,update on tst_lwx_ar_stmt_lines to apps;
 
 create table tst_lwx_ar_stmt_line_details as select * from lwx.lwx_ar_stmt_line_details where 0=1;
 grant select,insert,delete,update on tst_lwx_ar_stmt_line_details to apps;
-
-create or replace procedure
-auton_copy_stmt_to_tst_tbls(
