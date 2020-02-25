ece17037603571dbaf693ad4f3325949a551cd9d
Rich Stewart <rich.stewart@lifeway.com>
2020-02-25 12:10:15 -0600

    We need to distinguish test executions by a sequence and test-run date, so added an Oracle sequence (also owned by rstewar) to provide that numbering, and added a test-date column, to the tst_lwx_ar_stmt* tables.
    The package procedure
      autono_stmt_gen_test.copy_stmt_data
    also needed changes; for those, see other commits in this line of development.

--- test-harness/con-stmt-gen-testing.sql
+++ test-harness/con-stmt-gen-testing.sql
@@ -2,11 +2,22 @@
 /*
  Within the rstewar schema:
 */
+/* 
+This sequence is to be used to help distinguish test runs apart.
+We shall use it to label a test run within the stored procedure which
+copies data from the "main" statement-application tables into the
+"tst_lwx_ar*" tables created herein:
+*/
+create sequence stmt_tst_seq;
+
 create table tst_lwx_ar_stmt_headers as select * from lwx.lwx_ar_stmt_headers where 0=1;
 grant select,insert,delete,update on tst_lwx_ar_stmt_headers to apps;
+alter table tst_lwx_ar_stmt_headers add (test_date date, test_sequence number);
 
 create table tst_lwx_ar_stmt_lines as select * from lwx.lwx_ar_stmt_lines where 0=1;
 grant select,insert,delete,update on tst_lwx_ar_stmt_lines to apps;
+alter table tst_lwx_ar_stmt_lines add (test_date date, test_sequence number);
 
 create table tst_lwx_ar_stmt_line_details as select * from lwx.lwx_ar_stmt_line_details where 0=1;
 grant select,insert,delete,update on tst_lwx_ar_stmt_line_details to apps;
+alter table tst_lwx_ar_stmt_line_details add (test_date date, test_sequence number);
