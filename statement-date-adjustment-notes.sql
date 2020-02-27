1ca648d0a2f6ca99dc498b305e2f41c917c71088
Rich Stewart <rich.stewart@lifeway.com>
2020-02-27 17:03:05 -0600

    A example of how to use the statement-data-deletion package.

--- statement-date-adjustment-notes.sql
+++ statement-date-adjustment-notes.sql
@@ -53,3 +53,21 @@ break on stmt_line_id skip 1 dup
 select * from v_ar_stmt_info 
 where send_to_cust_nbr = :p_customer_nbr
 order by stmt_hdr_id, stmt_line_id, stmt_line_dtl_id, stmt_dte
+
+/*
+deletion of particular generated statement:
+this, praise God, can be invoked from a sqlplus prompt!
+*/
+declare 
+  l_error_msg varchar2(2000); 
+  l_error_code number; 
+begin
+  lwx_ar_delete_statements.delete_stmt(
+    p_error_msg => l_error_msg,
+    p_error_code => l_error_code,
+    p_stmt_hdr_id => 8503108, -- <<<< You must look this up.
+    p_request_id => null
+  );
+  dbms_output.put_line('Returned error msg:  '||substr(l_error_msg, 1,150));
+  dbms_output.put_line('Returned error code:  '||to_char(l_error_code, 'tm9'));
+end;
